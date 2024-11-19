0c6dba0bebdc99ecf11d26764572670a813c6a21
Farhat <farhatabbas.dev@gmail.com>
2024-11-20 02:33:41 +0500

    Added formik validation and  designed Registration page

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+import App from "./App.tsx";
+
+import { ThemeProvider } from "@material-tailwind/react";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </StrictMode>
+);
