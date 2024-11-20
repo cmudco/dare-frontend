4729fea65edba53d35e6c6177a334ebb5a4ba1b5
Farhat <farhatabbas.dev@gmail.com>
2024-11-20 21:41:00 +0500

    Feature: Registration Screen

--- src/pages/Registration/index.tsx
+++ src/pages/Registration/index.tsx
@@ -3,9 +3,14 @@ import RegistrationForm from "./RegistrationForm";
 
 const Registration = () => {
   return (
-    <div className='flex items-center justify-center min-h-screen bg-gradient-to-r from-pink-50 to-white'>
-      <RegistrationForm />
-    </div>
+    <>
+      <div className='absolute md:block hidden'>
+        <img src='/shapes/BgCircle.png' alt='' />
+      </div>
+      <div className='flex items-center justify-center min-h-screen '>
+        <RegistrationForm />
+      </div>
+    </>
   );
 };
 
