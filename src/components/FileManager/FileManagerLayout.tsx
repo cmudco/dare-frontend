634f2bbdf7623f4968c56c5a259a8d4f15d3252d
Farhat Abbas <farhxt.abbxs69@gmail.com>
2024-12-11 00:24:43 +0500

    misc: package-lock reappearing

--- src/components/FileManager/FileManagerLayout.tsx
+++ src/components/FileManager/FileManagerLayout.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { RootState, AppDispatch } from "../../redux/store";
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "../../redux/store";
 import {
   Card,
   CardBody,
