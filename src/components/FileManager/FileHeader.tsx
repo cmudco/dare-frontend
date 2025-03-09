a60bef6861b0c2c8a624d6c720ff266336065e7d
farhat-is-coding <farhxt.abbxs69@gmail.com>
2025-03-09 08:48:37 +0500

    refactor: replace material-tailwind with shadcn

--- src/components/FileManager/FileHeader.tsx
+++ src/components/FileManager/FileHeader.tsx
@@ -1,27 +1,25 @@
 import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
-import { Input, Button } from "@material-tailwind/react";
 import { useDispatch } from "react-redux";
 import { openModal } from "../../redux/fileSlice";
+import { Button } from "../UI/button";
+import { Input } from "../UI/input";
 
 const FileHeader: React.FC = () => {
     const dispatch = useDispatch();
 
     return (
         <div className='flex items-center justify-between px-2.5'>
-            <div className='w-[300px] h-[40px] relative flex items-center'>
-                <Input
-                    label='Search by tag'
-                    variant='outlined'
-                    className='bg-white'
-                    icon={
-                        <MagnifyingGlassIcon className='absolute h-5 w-5 text-gray-500' />
-                    }
-                    crossOrigin={false}
-                />
+            <div className='relative w-[300px] h-[40px] flex items-center'>
+                    <Input
+                        type="text"
+                        placeholder="Search by tag"
+                        className=" bg-white border border-gray-300 rounded-md focus:ring-2 focus:ring-primary focus:border-primary"
+                    />
+                    <MagnifyingGlassIcon className="absolute right-3 w-5 h-5 text-gray-500" />
             </div>
             <Button
                 className='bg-primary text-white py-2 rounded-md shadow-sm normal-case font-normal whitespace-nowrap !gap-6'
-                size='lg'
+                size='default'
                 onClick={() => dispatch(openModal())}
             >
                 Upload File
