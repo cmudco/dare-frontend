39674181a26a84aa92dbecfbce6ad721cb3e0f73
Farhat Abbas <74064529+farhat-is-coding@users.noreply.github.com>
2025-04-19 00:55:36 +0500

    feature: sentry integration (#26)
    * feat: add model context settings component and integrate with conversation settings
    
    * refactor: remove redundant resetConversation dispatch in ConversationHistory and update dependency array in ConversationLayout
    
    * feat: integrate Sentry for error tracking and monitoring

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,28 @@ import './index.css'
 import App from './App.tsx'
 import { Provider } from 'react-redux'
 import { store } from './redux/store.ts'
+import * as Sentry from '@sentry/react'
+
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration({
+      maskAllInputs: true,
+      blockAllMedia: true,
+    }),
+  ],
+
+  tracesSampleRate: 1.0,
+
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+})
 
 createRoot(document.getElementById('root')!).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <Sentry.ErrorBoundary fallback={<p>An error has occurred</p>}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </Sentry.ErrorBoundary>
 )
