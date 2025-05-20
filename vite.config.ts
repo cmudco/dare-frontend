bd6c0a222fe24036a250cdc0c0432fac42cefc56
Farhat Abbas <farhxt.abbxs69@gmail.com>
2025-05-20 23:08:57 +0500

    chore: remove sentry vite plugin for build issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { sentryVitePlugin } from '@sentry/vite-plugin'
 import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   build: {
-    sourcemap: true,
+    sourcemap: false,
   },
-  plugins: [
-    react(),
-    sentryVitePlugin({
-      org: process.env.VITE_SENTRY_ORG,
-      project: process.env.VITE_SENTRY_PROJECT,
-      authToken: process.env.VITE_SENTRY_AUTH_TOKEN,
-    }),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
