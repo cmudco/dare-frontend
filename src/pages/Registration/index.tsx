2a1e115f2f36ff1ad025d12b589a5ead76605289
Farhat Abbas <farhxt.abbxs69@gmail.com>
2025-07-17 00:25:48 +0500

    feat: implement dark mode toggle in header and login/registration screens

--- src/pages/Registration/index.tsx
+++ src/pages/Registration/index.tsx
@@ -1,21 +1,23 @@
 import React, { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import AuthCard from '../../components/Auth/AuthCard'
 import AuthFormFooter from '../../components/Auth/AuthFormFooter'
-import { AppDispatch } from '../../redux/store'
+import { AppDispatch, RootState } from '../../redux/store'
 import { userRegister } from '../../redux/asyncThunks/user'
 import {
   SignupFormValues,
   signupInitialValues,
   signupValidationSchema,
 } from './validation'
-
 import { resetError } from '../../redux/userSlice'
+import { Button } from '../../components/ui/button'
+import { Moon, Sun } from 'lucide-react'
 
 const RegistrationScreen: React.FC = () => {
   const navigate = useNavigate()
   const dispatch = useDispatch<AppDispatch>()
+  const isDarkMode = useSelector((state: RootState) => state.theme.isDarkMode)
 
   useEffect(() => {
     dispatch(resetError())
@@ -43,6 +45,11 @@ const RegistrationScreen: React.FC = () => {
     }
   }
 
+  const toggleTheme = () => {
+    const newTheme = isDarkMode ? 'light' : 'dark'
+    dispatch({ type: 'theme/setDarkMode', payload: newTheme === 'dark' })
+  }
+
   const formikConfig = {
     initialValues: signupInitialValues,
     validationSchema: signupValidationSchema,
@@ -62,22 +69,31 @@ const RegistrationScreen: React.FC = () => {
   ]
 
   return (
-    <AuthCard<SignupFormValues>
-      title='Create Account'
-      inputs={inputs}
-      formikConfig={formikConfig}
-      buttonText='Create Account'
-      showprivacyPolicy
-      footer={
-        <>
-          <AuthFormFooter
-            text='Already have an account?'
-            route='/login'
-            routeText='Log in'
-          />
-        </>
-      }
-    ></AuthCard>
+    <>
+      <div className='fixed right-6 top-6 z-10'>
+        <Button variant='outline' size='icon' onClick={toggleTheme}>
+          <Sun className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
+          <Moon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
+          <span className='sr-only'>Toggle theme</span>
+        </Button>
+      </div>
+      <AuthCard<SignupFormValues>
+        title='Create Account'
+        inputs={inputs}
+        formikConfig={formikConfig}
+        buttonText='Create Account'
+        showprivacyPolicy
+        footer={
+          <>
+            <AuthFormFooter
+              text='Already have an account?'
+              route='/login'
+              routeText='Log in'
+            />
+          </>
+        }
+      ></AuthCard>
+    </>
   )
 }
 
