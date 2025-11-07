4ba885c95a940260c4d5a4cd0c59aa288284d35e
farhat-is-coding <farhxt.abbxs69@gmail.com>
2025-11-08 00:14:37 +0500

    revert: workflow bug

--- src/pages/Workflows/_builder/WorkflowBuilder.tsx
+++ src/pages/Workflows/_builder/WorkflowBuilder.tsx
@@ -128,8 +128,7 @@ const WorkflowBuilder: React.FC<WorkflowBuilderProps> = (props) => {
               }
               if (
                 currentRun?.steps?.some(
-                  (step) =>
-                    step.stepNode && step.stepNode.toString() === node.id
+                  (step) => step.stepNode.toString() === node.id
                 )
               ) {
                 return '#10b981' // Green for completed
