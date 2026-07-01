db742c4e3e5c12df201281658045ffbafc91099a
m-Hariss <m.hariss123@gmail.com>
2026-07-01 17:01:20 +0500

    fix: update syftbox repo URL

--- src/components/WorkflowManager/ExportWorkflowDialog.tsx
+++ src/components/WorkflowManager/ExportWorkflowDialog.tsx
@@ -10,7 +10,7 @@ import {
 import { Button } from '../ui/button'
 import { FileDown, Copy, Check, Info } from 'lucide-react'
 
-const SYFTBOX_APP_URL = 'https://github.com/m-Hariss/dare-workflows.git'
+const SYFTBOX_APP_URL = 'https://github.com/cmudco/dare-syftbox-workflow/'
 
 interface ExportWorkflowDialogProps {
   isOpen: boolean
